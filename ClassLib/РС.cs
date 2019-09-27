0dc8ca9e0933afa3117e957a8d0974181d4ff35e
makishev@yahoo.com <makishev@yahoo.com>
2019-09-27 21:43:33 +0600

    pract

---
+++ ClassLib/РС.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLib
+{
+    public class PC
+    {
+        //поля(марка, серийный номер и т.д.),  
+        //свойства(к каждому полю),  
+        //конструкторы(по умолчанию, с параметрами),  
+        //методы, моделирующие функционирование ПЭВМ
+
+        public string PcModel { get; set; }
+        public string SerialNumber { get; set; }
+        public string Info { get; set; }
+        public double Price { get; set; }
+
+        public PC()
+        {
+            PcModel = "GamePC"; SerialNumber = "0x000"; Info = "Intel Core i9 4000Mhz"; Price = 1000;
+        }
+
+        public PC(string pcModel, string serialNumber, string info, double price)
+        {
+            PcModel = pcModel; SerialNumber = serialNumber; Info = info; Price = price;
+        }
+
+        public double GiveMeSumTwoNumbers(double leftNumber, double rightNumber)
+        {
+            double sum = leftNumber + rightNumber;
+            return sum;
+        }
+        public void PCstart()
+        {
+            Console.WriteLine("Hello BOSS!!!");
+        }
+        public void PCoff()
+        {
+            Console.WriteLine("GoodBye BOSS!!!");
+        }
+        public void PCsleep()
+        {
+            Console.WriteLine(this.PcModel + " Going to sleep");
+        }
+    }
+}
---
+++ SerializConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+
+
+namespace ClassLib
+{
+    class Program<T>
+    {
+        string path = @"C:\listSerial.txt";
+        static void Main(string[] args)
+        {
+            List<PC> computers = new List<PC>
+            {
+                new PC
+                {
+                    PcModel = "WorkPC",
+                    SerialNumber = "0x005656A",
+                    Info = "Компьютер для офиса",
+                    Price = 250000
+                },
+                new PC
+                {
+                    PcModel = "HomePC",
+                    SerialNumber = "0x01056A",
+                    Info = "Компьютер для дома",
+                    Price = 312000
+                },
+                new PC
+                {
+                    PcModel = "GamePC",
+                    SerialNumber = "0x01100B",
+                    Info = "Игровой компьютер",
+                    Price = 545000
+                },
+                new PC
+                {
+                    PcModel = "HardcoreGamePC",
+                    SerialNumber = "0x01110C",
+                    Info = "Компьютер для заядлых игроманов",
+                    Price = 1255000
+                }
+            };
+
+            string json = JsonConvert.SerializeObject(computers);
+
+            using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(path: @"C:\listSerial.txt", FileMode.CreateNew)))
+            {
+                binaryWriter.Write(json);
+            }
+
+            //var result = JsonConvert.DeserializeObject<List<Book>>(json);
+        }
+    }
+}
